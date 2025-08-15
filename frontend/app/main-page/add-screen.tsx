7827935af0e103ccd13e4c1623dd8620208f1bfa
Xavier <xman123475@gmail.com>
2025-08-15 15:01:31 -0700

    added keyboard

--- frontend/app/main-page/add-screen.tsx
+++ frontend/app/main-page/add-screen.tsx
@@ -1,5 +1,9 @@
 import react from "react"
 import MyDropdown from "../components/MyDropdown"
+import React, { useState } from "react";
+import Keyboard from "react-simple-keyboard";
+import "react-simple-keyboard/build/css/index.css";
+import "../keyboard.css"
 interface addScreenProps {
     navigateToListClick,
     currTaskName,
@@ -13,6 +17,7 @@ interface addScreenProps {
     addTaskButtonHandler
 }
 
+
 // Add: form to add task
 const AddScreen = 
     ({navigateToListClick,
@@ -25,34 +30,57 @@ const AddScreen =
     monthOrWeek,
     setMonthlyOrWeeklyCheck,
     addTaskButtonHandler}: addScreenProps) => {
+    const [showKeyboard, setShowKeyboard] = useState(false)
     return (
-        <div className="w-2/3 bg-[#1C1E1F]">               
-            <div className="relative">
-                <button onClick={navigateToListClick} className="bg-gradient-to-l from-blue-500 via-blue-600 to-blue-700 font-medium rounded-lg text-sm text-white h-10.5 w-12 text-center absolute left-3 top-3 ">Back</button>
-            </div>
-            <div className="flex flex-col h-screen items-center justify-center space-y-4" >
-                {/* task title and year TODO: maybe add a today option*/}
-                <div className="flex items-center space-x-4">
-                    <div className="flex flex-col space-y-4">
-                        <label className="text-white" >Task Title</label>
-                        <label className="text-white" >Day</label>
-                        <label className="text-white" >Current Day?</label>
-                        <label className="text-white" >Repeating </label>
-                    </div>
-                    <div className="flex flex-col space-y-3">
-                        <input required value={currTaskName} onChange={e => setCurrTaskName(e.target.value)} type="text" id="taskName" name="taskName" className="p-2 border border-white rounded w-50 h-8 text-white " />
-                        <input required value={currTaskDate} onChange={e => setCurrTaskDate(e.target.value)} type="date" id="taskDate" name="taskDate" className="p-2 border border-white rounded w-35 h-8 text-white" />
-                        <input type="checkbox" checked={isTodayChecked} onChange={handleTodayCheckbox} className="size-5 accent-blue-300"></input>
-                        <MyDropdown arr={monthOrWeek} title="MonthOrWeek" setSelected={setMonthlyOrWeeklyCheck}></MyDropdown>
-                    </div>
+            <div className="w-2/3 bg-[#1C1E1F] h-screen">               
+                <div className="flex flex-col space-y-4 h-screen items-center justify-center" >
+                    <div className="grid grid-cols-2 items-center">
+                        <div className="flex flex-col space-y-3">
+                            <label className="text-white" >Task Title</label>
+                            <label className="text-white" >Day</label>
+                            <label className="text-white" >Current Day?</label>
+                            <label className="text-white" >Repeating </label>
+                        </div>
+                        <div className="flex flex-col space-y-3">
+                            {/* input field for task title */}
+                            <input 
+                                required value={currTaskName} 
+                                onFocus={() => setShowKeyboard(!showKeyboard)}
+                                onChange={e => setCurrTaskName(e.target.value)} 
+                                type="text" 
+                                id="taskName" 
+                                name="taskName" 
+                                className="p-2 border border-white rounded w-50 h-8 text-white " 
+                            />
+                            <input required value={currTaskDate} onChange={e => setCurrTaskDate(e.target.value)} type="date" id="taskDate" name="taskDate" className="p-2 border border-white rounded w-35 h-8 text-white" />
+                            <input type="checkbox" checked={isTodayChecked} onChange={handleTodayCheckbox} className="size-5 accent-blue-300"></input>
+                            <MyDropdown arr={monthOrWeek} title="MonthOrWeek" setSelected={setMonthlyOrWeeklyCheck}></MyDropdown>
+                        </div>
 
+                    </div>
+                    {/* add task button */}
+                    <div className="pt-1 flex gap-x-5">
+                        <button onClick={navigateToListClick} className="bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 font-medium rounded-lg text-sm text-white h-10.5 w-25 text-center">Back</button>
+                        <button onClick={addTaskButtonHandler} className="bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 font-medium rounded-lg text-sm text-white h-10.5 w-25 text-center">Add Task</button>
+                    </div>
                 </div>
-                {/* add task button */}
-                <div className="flex items-center">
-                    <button onClick={addTaskButtonHandler} className="bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 font-medium rounded-lg text-sm text-white h-10.5 w-25 text-center">Add Task</button>
+                <div
+                    className={`fixed bottom-0 right-0 transition-transform duration-300 ease-in-out w-full h-1/2  ${
+                        showKeyboard ? "translate-y-0 opacity-100" : "translate-y-full opacity-0"
+                    }`}
+                    >
+                    <Keyboard
+                        onChange={(input) => {
+                        setCurrTaskName(input);
+                        }}
+                        onKeyPress={(button) => {
+                        if (button === "{enter}") {
+                            setShowKeyboard(false);
+                        }
+                        }}
+                    />
                 </div>
             </div>
-        </div>
     ) 
 }
 
---
+++ node_modules/react-simple-keyboard/build/components/KeyboardModern.d.ts
@@ -0,0 +1,4 @@
+import "simple-keyboard/build/css/index.css";
+import { KeyboardReactInterface } from "../interfaces";
+declare const KeyboardReact: (props: KeyboardReactInterface["options"]) => any;
+export default KeyboardReact;
